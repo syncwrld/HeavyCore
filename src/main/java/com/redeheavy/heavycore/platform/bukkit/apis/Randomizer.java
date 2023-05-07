a54c485dc1bdd1d010f23c4a1e794416db3aeed6
sync <85231933+syncwrld@users.noreply.github.com>
2023-05-07 11:31:10 -0300

    deleted all files to update
    ...

--- src/main/java/com/redeheavy/heavycore/platform/bukkit/apis/Randomizer.java
+++
@@ -1,9 +0,0 @@
-package com.redeheavy.heavycore.platform.bukkit.apis;
-
-public class Randomizer {
-
-    public static int generate(int min, int max) {
-        return min + (int) (Math.random() * ((max - min) + 1));
-    }
-
-}
